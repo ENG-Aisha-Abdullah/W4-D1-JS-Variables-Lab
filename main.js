6969bccc2ad7574c8d4dd010fda278adf2ccbdc1
Ayoosh <>
2025-05-04 12:49:56 +0300

    task 1+ 3 + 4

--- main.js
+++ main.js
@@ -3,3 +3,13 @@ let y = 2;
 let area = x + y;
 let result = `The area of the rectangle is ${area}`;
 console.log(result);
+
+let hours = 1
+let Seconds = 60*60
+let time = `${hours} hours = ${Seconds} Seconds `;
+console.log(time);
+
+
+let thingOne ="thingOne";
+let thingTwo ="thingTwo";
+console.log(thingOne + thingTwo);
